e4011969fe5cf63cfdb4c0fe9674082870e19bb3
Andrea Falbo <andreafalbo46@gmail.com>
2026-06-03 19:06:33 +0200

    style: new favicon, metadata added

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Metadata } from 'next'
 import { Header } from "@/components/header"
 import { Hero } from "@/components/hero"
 import { Partners } from "@/components/partners"
@@ -8,6 +9,11 @@ import { Lorenzo } from "@/components/lorenzo"
 import { Contatti } from "@/components/contatti"
 import { Footer } from "@/components/footer"
 import { Ringraziamenti } from "@/components/ringraziamenti"
+
+export const metadata: Metadata = {
+    title: "Mediterraneo in Rosa | Raid Solidale in Gommone per le Donne",
+    description: "Il raid in gommone dalle donne per le donne a favore di “Cuore Rosa”: il ventaglio di servizi gratuiti dedicati alle donne che affrontano una diagnosi oncologica, firmato Associazione Lorenzo Perrone.",
+}
 
 export default function Home() {
   return (
